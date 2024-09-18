4b435973247f5cfddfacda064b3989119743914a
albertprofe <albertprofe@gmail.com>
2024-09-18 12:30:43 +0200

    testOrder() and printStatusTable() in Utilities

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -14,39 +14,21 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("\nInit");
+        System.out.println("\nInit\n");
 
         RestaurantDB r1 = new RestaurantDB();
 
         //create RestaurantDB r1;
-        Utilities.createFakeMenus();
-        Utilities.createTables(r1);
+        Utilities.createFakeMenus(r1);
+        Utilities.createFakeTables(r1);
 
-        testOrder(r1);
-
+        Utilities.testOrder(r1);
+        Utilities.printStatusTable(r1);
 
         System.out.println("\nFinish");
     }
 
-    public static void testOrder(RestaurantDB r1){
 
-        ArrayList<Menu> m = new ArrayList<>();
-       // m.add(m1);
-       // m.add(m1);
-       // m.add(m2);
-       // m.add(m3);;
-        Order o1 = new Order(new Date(), "Jazz", 20,
-                0.0, false, r1.getTables().get("T1"), null);
-        o1.setMenus(m);
-
-        System.out.println("Total to pay:"+ o1.calculateTotalPayment());
-        System.out.println(o1);
-
-        System.out.println("");
-
-
-
-    }
 
 
 
--- src/main/java/org/example/utils/Utilities.java
+++ src/main/java/org/example/utils/Utilities.java
@@ -1,15 +1,16 @@
 package org.example.utils;
 
 import org.example.model.Menu;
+import org.example.model.Order;
 import org.example.model.Table;
 import org.example.repository.RestaurantDB;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Utilities {
 
-
-    public static void createTables(RestaurantDB r1){
+    public static void createFakeTables(RestaurantDB r1){
         // create 5 tables
         Table t1 = new Table("Table 01", "Table type Modern", 4, false);
         Table t2 = new Table("Table 02", "Table type Modern", 2, false);
@@ -18,17 +19,67 @@ public class Utilities {
         Table t5 = new Table("Table 05", "Table type Modern", 4, false);
         //save tables to r1.tables HashMap tables
 
-        r1.getTables().put("T1", t1);
+        r1.getTables().put("TABLE-01", t1);
+        r1.getTables().put("TABLE-02", t2);
+        r1.getTables().put("TABLE-03", t3);
+        r1.getTables().put("TABLE-04", t4);
+        r1.getTables().put("TABLE-05", t5);
 
     }
 
-
-    public static void createFakeMenus(){
+    public static void createFakeMenus(RestaurantDB r1){
         Menu m1 = new Menu("Menu Night", 8.5, "", true, true);
         Menu m2 = new Menu("Menu Vegan", 10.5, "", true, true);
         Menu m3 = new Menu("Menu Kids", 12.5, "", true, true);
-        // save to HashMap ...
 
+        r1.getMenus().put("MENU-NIG", m1);
+        r1.getMenus().put("MENU-VEG", m2);
+        r1.getMenus().put("MENU-KID", m3);
+
+    }
+
+    public static void testOrder(RestaurantDB r1){
+
+        ArrayList<Menu> m = new ArrayList<>();
+        m.add(r1.getMenus().get("MENU-NIG"));
+        m.add(r1.getMenus().get("MENU-NIG"));
+        m.add(r1.getMenus().get("MENU-VEG"));
+        m.add(r1.getMenus().get("MENU-KID"));
+
+        Order o1 = new Order(new Date(), "Jazz", 4,
+                0.0, false, r1.getTables().get("TABLE-01"), null);
+        o1.setMenus(m);
+
+        r1.getTables().get("TABLE-01").setBusy(true);
+
+        System.out.println("Total to pay:"+ o1.calculateTotalPayment());
+        System.out.println(o1);
+
+        o1.setPaid(true);
+
+        System.out.println(o1);
+        r1.getTables().get("TABLE-01").setBusy(false);
+
+        System.out.println("Table status (" +
+                r1.getTables().get("TABLE-01").getName() +
+                "):" + r1.getTables().get("TABLE-01").isBusy());
+
+
+    }
+
+    public static void printStatusTable(RestaurantDB r1) {
+
+        System.out.println("\n\nAvailable tables:\n");
+        r1.getTables().get("TABLE-01").setBusy(true);
+        for ( Table t : r1.getTables().values() ){
+
+            if(!t.isBusy()) {
+                System.out.println(t.getName());
+            }
+        }
+        //for each tables hash
+        // if table BUSY is available  PRINT
+        //
 
 
 
