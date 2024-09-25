c723947f6634bd5fb7f040c963fed1e8128e4a76
albertprofe <albertprofe@gmail.com>
2024-09-25 10:57:33 +0200

    createOrder() v3.4

--- src/main/java/org/example/manager/OrderManager.java
+++ src/main/java/org/example/manager/OrderManager.java
@@ -55,76 +55,25 @@ public class OrderManager {
     public static boolean createOrder(Scanner scanner, RestaurantDB r1){
 
         boolean statusOperation  = false;
+
         // create object
         Order order1 = new Order();
-
+        // create date
         order1.setDate(new Date());
-
         // create waiter
-        //String waiter = Utilities.ask(scanner, "Waiter? ");
-        //order1.setWaiter(waiter);
-
-        // create waiter
-
         order1.setWaiter(Utilities.ask(scanner, "Waiter? "));
-
-   /*     // people qty
-        while (true) {
-            String qty = Utilities.ask(scanner, "People qty? ");
-            try {
-                int qtyInt = Integer.parseInt(qty);
-                order1.setPeopleQty(qtyInt);
-                break; // Exit the loop if input is valid
-            } catch (NumberFormatException ex) {
-                System.out.println("Invalid input. Please enter an integer.");
-            }
-        }*/
-
+        //Create people qty
         order1.setPeopleQty(qtyPeople(scanner));
-
-
         // create table
         order1.setTable(selectTable(r1, scanner));
-        /*System.out.println("\nSelect table:");
-
-        TableManager.printAvailableTables(r1);
-        System.out.println("0 - Take Away");
-        String tableSelection = Utilities.ask(scanner, "Table? ");
-
-        if (tableSelection.equals("0")) order1.setTable(null);
-        else
-            order1.setTable(r1.getTables().get(tableSelection));*/
-
-
         // create menus
-       /* System.out.println("\nSelect menus:");
-        ArrayList<Menu> menus = new ArrayList();
-        while(true) {
-
-            System.out.println("0 - Quit");
-            r1.getMenus().forEach((key, menu) -> {
-                // if menu is active
-                System.out.println( key + " - " + menu.getName());
-            });
-
-            String option = Utilities.ask(scanner, "Menu? ");
-            if (option.equals("0")){ break; }
-            else {
-                menus.add(r1.getMenus().get(option));
-            }
-
-        }*/
         order1.setMenus(selectMenus(r1, scanner));
-
-
         // total payment
         order1.setTotalPayment(order1.calculateTotalPayment());
-
         // create paid
         order1.setPaid(false);
 
-
-        // saver order to repo
+        // save order to repo
         String uuid = UUID.randomUUID().toString();
         r1.getOrders().put(uuid, order1);
         Order orderSaved = r1.getOrders().get(uuid);
@@ -135,10 +84,6 @@ public class OrderManager {
             System.out.println("\nOrder");
             System.out.println("Order ID: " + uuid);
             System.out.println(orderSaved);
-
-            //r1.getTables().get(tableSelection).setBusy(true);
-            //System.out.println(  "\nTable status BUSY(" +  r1.getTables().get(tableSelection).getName() + "):" + r1.getTables().get(tableSelection).isBusy());
-            //TableManager.printAvailableTables(r1);
         }
 
         return statusOperation;
